357d82e735db5dfb9ca200fc3f149904b955dfc4
Rafał <raflp@dev-effectivity.pl>
2020-11-06 20:40:08 +0100

    Added typescript

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -2,11 +2,12 @@ import { staffRoomUrl, staffRoomStartUrl } from '../config';
 import { expect } from "chai";
 
 export default class LoginPage {
+  private page;
   constructor(page) {
     this.page = page
   }
 
-  async login(user_id, user_password) {
+  async login(user_id: string, user_password: string) {
     await this.page.goto(staffRoomUrl)
     await this.page.waitAndType("#form-login > div > input[name='login_email']", user_id)
     await this.page.waitAndType("#form-login > div > input[name='login_password']", user_password)
